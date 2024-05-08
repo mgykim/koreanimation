24e08f1285a60ab668215b3f44bd35def26e917b
Chenny Yun <sy3031@nyu.edu>
2024-05-08 00:43:42 -0400

    updated baby animation with sounds

--- scripts/script.js
+++ scripts/script.js
@@ -2,46 +2,53 @@ document.addEventListener('DOMContentLoaded', function () {
     console.log('JavaScript loaded!');
 });
 
+
 var animation = bodymovin.loadAnimation({
     container: document.getElementById('animationContainer'), // the dom element that will contain the animation
     renderer: 'canvas', // Render type: 'canvas', 'html'
     loop: false, // loop the animation
     autoplay: true, // start playing the animation as soon as it is loaded
-    path: '../animations/baby.json' // the path to the animation json
+    path: '../animations/baby.json', // the path to the animation json
   });
 
+// getAudioContext().resume();
+
 const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
-var playIdle = false;
-
-
-const continueAnimation = async () => {
-  animation.play();
-  event.target.removeEventListener("mousedown",pauseAnimation);
-  event.target.removeEventListener("mouseup",continueAnimation);
-}
-
-const playIdleAnimation = async () => {
-  animation.play();
-  await sleep(timingArray[currentIndex]);
-  animation.goToAndPlay(13000,false);
-}
-
-const pauseAnimation =  async () =>{
-  animation.play();
-  await sleep('200');
-  animation.pause();
-}
 var animationDiv = document.getElementById('animationContainer');
+var pacifierDiv = document.getElementById('pacifier');
+
+const pauseAndPlay =  async () =>{
+  animation.play();
+  await sleep(250);
+  animation.pause();
+  animation.play();
+  animationDiv.removeEventListener("mousedown",pauseAndPlay);
+  animationDiv.addEventListener("click", stopIdle);
+  playIdle();
+}
 
 const startAnimation = async () => {
-    await sleep(4000);
+    await sleep(4100);
     animation.pause();
-    animationDiv.addEventListener("mousedown",pauseAnimation);
-    animationDiv.addEventListener("mouseup",continueAnimation);
+    animationDiv.addEventListener("mousedown",pauseAndPlay);
+}
+var idleInterval; // Variable to hold the interval ID
+var idleCheck = true;
+
+const playIdle = () => {
+  idleInterval = setInterval(() => {
+    if (idleCheck) {
+      animation.playSegments([457,476], true);
+    } else {
+      clearInterval(idleInterval); // Clear the interval if idleCheck is false
+    }
+  }, 1000); // Interval set to 1000 milliseconds (1 second)
 }
 
-startAnimation();
+const stopIdle = () => {
+  idleCheck = false;
+  clearInterval(idleInterval); // Ensure to clear the interval when stopping the idle
+}
 
-// if (playIdle){
-//   playIdleAnimation();
-// }
+
+startAnimation();
